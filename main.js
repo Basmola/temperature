37033b7654b31b97ea1899328cc2bb6d39e73a33
Basmola <basmalaamr9@gmail.com>
2024-10-15 01:44:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let button1=document.getElementById("btn");
+let button2=document.getElementById("btn-reset");
+let input=document.getElementById("input");
+let output=document.querySelector(".output");
+button1.onclick = () =>{
+    let inputValue = input.value;
+    if (isNaN(inputValue) || inputValue === ""){
+        output.innerHTML = "Enter a valid temperature";
+    }
+    else{
+        let outputValue=inputValue * ( 9 / 5 ) +32;
+        output.innerHTML=`The Tempreture in Fahrenheit:${outputValue} °F`;
+    }
+    };
+button2.onclick = () =>{
+output.innerHTML="";
+}
